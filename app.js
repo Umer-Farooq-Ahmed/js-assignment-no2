10fdaf94037e2f5fa698a013bb47679b1f14cbfc
Umer-FarooqAhmed <123248058+Umer-Farooq-Ahmed@users.noreply.github.com>
2023-01-30 01:47:40 +0500

    Js.Assignment.No2.JavaScript.file
    Js.Assignment.No2.JavaScript.file

---
+++ app.js
@@ -0,0 +1,95 @@
+//**** */ Chapter no2
+
+// Question no 1
+
+// var myname
+
+// Question no 2
+
+// var myname ="my name is farooq"
+
+// Question no 3
+
+// var myname ="hello world"
+
+// alert(myname)
+
+// Question no4
+
+// var myname ="my name is farooq"
+
+// alert(myname)
+
+// Question no5
+
+// var myname ="pizza /n pizz /n piz /n pi /n p"
+
+// alert(myname)
+
+// Question no6
+
+// var myname ="my email address 'm.farooq17022@email.com'"
+
+// alert(myname)
+
+// Question no7
+
+// var a ='"A Smart Way of learn JavaScript"'
+
+// alert(a)
+
+// Question no8
+
+// document.write("yah! i can write HTML conten through JavaScript")
+
+// Question no9
+
+// var a ='“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”'
+
+// alert(a)
+
+// ****** Chapter no3
+
+// Question no1
+
+//  var age ="I am 20years old"
+
+//  alert(age)
+
+// Question no2
+
+// var a ="you have visited this site 14 times"
+
+//  alert(a)
+
+// Question  no3
+
+// document.write("My birth year is 2002")
+
+// Question no4
+
+// var storename= "clothing store"
+
+// alert(storename)
+
+// document.write("“John Doe ordered 5 T-shirt(s) on XYZ Clothing store”.")
+
+// ***** Chapter no4
+
+// Question no1
+
+// var name "farooq"
+// var fullname "muhammad farooq"
+// var myname "farooq"
+
+// Question no2
+
+// var legalname "name.name.name.name.name"
+
+// var ilegalname "****.*****.*****.*****.*****"
+
+// Question no3
+
+// h1 ="rules for naming js variable"
+
+// document.write("a) A heading stating “Rules for naming JS variables” b) Variable names can only contain ______, ______,______ and ______.For example $my_1stVariable /n c) Variables must begin with a ______, ______ or/n _____. For example $name, _name or name/nd) Variable names are case _________/ne) Variable names should not be JS _________")
